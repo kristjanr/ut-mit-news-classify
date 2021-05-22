29fba2f7096ac20ac7efb1b94c1cf19a9012840d
Mykyta Baliesnyi <mykyta@rocket.hpc.ut.ee>
2021-05-22 14:57:46 +0300

    add all just in case

--- NYT/ensemble/vectorize-slurm.sh
+++ NYT/ensemble/vectorize-slurm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Job Details
-#SBATCH -J tfidf
+#SBATCH --partition=gpu
+#SBATCH -J ensemble
 
 # Resources
 #SBATCH -t 07:59:00
 #SBATCH --ntasks=1
 #SBATCH --mem=60G
 #SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:tesla:1
 
 # Actual job command(s)
 srun python vectorize.py
